7c5454aa918eafc5e51c02bb13c12ef4d4232f58
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-04-20 12:59:27 +0530

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -17,7 +17,7 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
       <nav className="flex items-center justify-between px-6 py-4 shadow">
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100 text-xl font-semibold">Deathrow</h2>
+          <h2 className="text-primary-100 text-xl font-semibold">Interview Saathi</h2>
         </Link>
       </nav>
 
