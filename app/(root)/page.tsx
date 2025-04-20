1c04d083f8c03cb495f2508b7bf4efa2f721bcc5
aritrachatterjee7 <161926590+aritrachatterjee7@users.noreply.github.com>
2025-04-20 11:15:23 +0530

    thalaaaa*2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -65,7 +65,7 @@ const Page = async() => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take Interviews</h2>
 
-        <div className="interviews-section">
+        {/* <div className="interviews-section">
           {hasUpcomingInterviews ? (
             allInterview?.map((interview) => (
               <InterviewCard
@@ -81,7 +81,7 @@ const Page = async() => {
           ) : (
             <p>There are no interviews available</p>
           )}
-        </div>
+        </div> */}
       </section>
     </>
   );
